842c8c66070307fd3da1335fb1cf1a3068002a3c
Your Name <you@example.com>
2023-03-20 03:24:33 +0300

    vector

--- src/s21_vector.h
+++ src/s21_vector.h
@@ -1,11 +1,6 @@
 #ifndef SRC_S21_VECTOR_H
 #define SRC_S21_VECTOR_H
 
-#include <cstring>
-#include <initializer_list>
-#include <iostream>
-#include <utility>
-
 namespace s21 {
 template <class T>
 class vector {
@@ -14,7 +9,6 @@ class vector {
   size_t m_capacity;
   T *arr;
 
- public:
   using value_type = T;
   using reference = T &;
   using const_reference = const T &;
@@ -22,37 +16,91 @@ class vector {
   using const_iterator = const T *;
   using size_type = size_t;
 
- private:
-  void reserve_more_capacity(size_type size);
+  void set_private_fields_(size_type size, size_type capacity,
+                           value_type *arr) {
+    m_size = size;
+    m_capacity = capacity;
+    this->arr = arr;
+    if (!arr && capacity) this->arr = new value_type[capacity];
+  }
+
+  void copy_vector(const vector &v) noexcept {
+    if (arr && m_capacity >= v.m_size)
+      for (size_type i = 0; i < v.m_size; ++i) arr[i] = v.arr[i];
+  }
+
+  void check_bounds_(size_type pos) const {
+    if (pos >= m_size)
+      throw std::out_of_range("Incorrect input, index is outside the vector");
+  }
+
+  void delete_value_() {
+    delete[] arr;
+    set_private_fields_(0U, 0U, nullptr);
+  }
 
  public:
-  vector() : m_size(0U), m_capacity(0U), arr(nullptr) {}
-  explicit vector(size_type n)
-      : m_size(n), m_capacity(n), arr(n ? new T[n] : nullptr) {}
-  explicit vector(std::initializer_list<value_type> const &items)
-      : m_size(items.size()),
-        m_capacity(items.size()),
-        arr(items.size() ? new T[items.size()] : nullptr) {
+  vector() { set_private_fields_(0U, 0U, nullptr); }
+
+  explicit vector(size_type n) { set_private_fields_(n, n, nullptr); }
+
+  explicit vector(std::initializer_list<value_type> const &items) {
+    size_type n;
+
+    n = items.size();
+    set_private_fields_(n, n, nullptr);
     if (arr)
-      for (auto i = 0, it = items.begin(); it != items.end(); i++, it++)
-        arr[i] = *it;
+      for (auto i = 0, item = items.begin(), end = items.end(); item < end;
+           i++, item++)
+        arr[i] = *item;
   }
-  vector(const vector &v)
-      : m_size(v.m_size),
-        m_capacity(v.m_capacity),
-        arr(v.m_size ? new T[v.m_size] : nullptr) {
-    if (arr) std::memcpy(arr, v.arr, m_size * sizeof(T));
-  };
-  vector(vector &&v) : m_size(v.m_size), m_capacity(v.m_capacity), arr(v.arr) {
-    v.m_size = 0;
-    v.m_capacity = 0;
-    v.arr = nullptr;
+
+  vector(const vector &v) {
+    set_private_fields_(v.m_size, v.m_capacity, nullptr);
+    copy_vector(v);
   }
+
+  vector(vector &&v) {
+    set_private_fields_(v.m_size, v.m_capacity, v.arr);
+    v.set_private_fields_(0U, 0U, nullptr);
+  }
+
   ~vector() { delete[] arr; }
+
+  vector &operator=(const vector &v) {
+    if (this != &v) {
+      delete[] arr;
+      set_private_fields_(v.m_size, v.m_capacity, nullptr);
+      copy_vector(v);
+    }
+
+    return *this;
+  }
+
+  vector &operator=(vector &&v) {
+    delete_value_();
+    if (this != &v) {
+      set_private_fields_(v.m_size, v.m_capacity, v.arr);
+      v.set_private_fields_(0U, 0U, nullptr);
+    }
+
+    return *this;
+  }
+
+  reference at(size_type pos) {
+    check_bounds_(pos);
+
+    return arr[pos];
+  }
+
+  reference at(size_type pos) const {
+    check_bounds_(pos);
+
+    return arr[pos];
+  }
 
   size_type size() { return m_size; }
   size_type capacity() { return m_capacity; }
-  value_type at(size_type i) { return arr[i]; }
   void push_back(value_type v);
 };
 }  // namespace s21
--- src/tests.cpp
+++ src/tests.cpp
@@ -7,8 +7,6 @@ TEST(TestS21Containers, Vector) {
   s21::vector<int> s21_default_constructor;
   ASSERT_EQ(std_default_constructor.size(), s21_default_constructor.size());
   ASSERT_EQ(std_default_constructor.capacity(), s21_default_constructor.capacity());
-//  ASSERT_THROW(std_default_constructor.at(0), std::out_of_range);
-//  ASSERT_THROW(s21_default_constructor.at(0), std::out_of_range);
 
   std::vector<int> std_parameterized_constructor(100);
   s21::vector<int> s21_parameterized_constructor(100);
@@ -22,30 +20,90 @@ TEST(TestS21Containers, Vector) {
   for (size_t i = 0; i < std_initializer_list_constructor.size(); i++)
     ASSERT_EQ(std_initializer_list_constructor.at(i), s21_initializer_list_constructor.at(i));
 
+  std::vector<int> std_copy_constructor(std_initializer_list_constructor);
   s21::vector<int> s21_copy_constructor(s21_initializer_list_constructor);
+  ASSERT_EQ(std_copy_constructor.size(), s21_copy_constructor.size());
+  ASSERT_EQ(std_copy_constructor.capacity(), s21_copy_constructor.capacity());
   ASSERT_EQ(s21_initializer_list_constructor.size(), s21_copy_constructor.size());
   ASSERT_EQ(s21_initializer_list_constructor.capacity(), s21_copy_constructor.capacity());
+  for (size_t i = 0; i < std_copy_constructor.size(); i++)
+    ASSERT_EQ(std_copy_constructor.at(i), s21_copy_constructor.at(i));
   for (size_t i = 0; i < s21_initializer_list_constructor.size(); i++)
     ASSERT_EQ(s21_initializer_list_constructor.at(i), s21_copy_constructor.at(i));
 
+  std::vector<int> std_move_constructor(std::move(std_initializer_list_constructor));
   s21::vector<int> s21_move_constructor(std::move(s21_initializer_list_constructor));
+  ASSERT_EQ(std_move_constructor.size(), s21_move_constructor.size());
+  ASSERT_EQ(std_move_constructor.capacity(), s21_move_constructor.capacity());
   ASSERT_EQ(s21_copy_constructor.size(), s21_move_constructor.size());
   ASSERT_EQ(s21_copy_constructor.capacity(), s21_move_constructor.capacity());
+  for (size_t i = 0; i < std_move_constructor.size(); i++)
+    ASSERT_EQ(std_move_constructor.at(i), s21_move_constructor.at(i));
   for (size_t i = 0; i < s21_copy_constructor.size(); i++)
     ASSERT_EQ(s21_copy_constructor.at(i), s21_move_constructor.at(i));
+  ASSERT_EQ(0, std_initializer_list_constructor.size());
+  ASSERT_EQ(0, std_initializer_list_constructor.capacity());
   ASSERT_EQ(0, s21_initializer_list_constructor.size());
   ASSERT_EQ(0, s21_initializer_list_constructor.capacity());
-/* 
-  s21::vector<int> operator_overload;
-  operator_overload = std::move(s21_move_constructor);
+
+  std::vector<int> std_operator_overload;
+  s21::vector<int> s21_operator_overload;
+  std_operator_overload = std_move_constructor;
+  s21_operator_overload = s21_move_constructor;
+  ASSERT_EQ(std_operator_overload.size(), s21_operator_overload.size());
+  ASSERT_EQ(std_operator_overload.capacity(), s21_operator_overload.capacity());
+  ASSERT_EQ(s21_move_constructor.size(), s21_operator_overload.size());
+  ASSERT_EQ(s21_move_constructor.capacity(), s21_operator_overload.capacity());
+  for (size_t i = 0; i < std_operator_overload.size(); i++)
+    ASSERT_EQ(std_operator_overload.at(i), s21_operator_overload.at(i));
+  for (size_t i = 0; i < s21_copy_constructor.size(); i++)
+    ASSERT_EQ(s21_move_constructor.at(i), s21_operator_overload.at(i));
+
+  std_operator_overload = std::move(std_move_constructor);
+  s21_operator_overload = std::move(s21_move_constructor);
+  ASSERT_EQ(std_operator_overload.size(), s21_operator_overload.size());
+  ASSERT_EQ(std_operator_overload.capacity(), s21_operator_overload.capacity());
   ASSERT_EQ(s21_copy_constructor.size(), s21_operator_overload.size());
   ASSERT_EQ(s21_copy_constructor.capacity(), s21_operator_overload.capacity());
+  for (size_t i = 0; i < std_operator_overload.size(); i++)
+    ASSERT_EQ(std_operator_overload.at(i), s21_operator_overload.at(i));
   for (size_t i = 0; i < s21_copy_constructor.size(); i++)
     ASSERT_EQ(s21_copy_constructor.at(i), s21_operator_overload.at(i));
+  ASSERT_EQ(0, std_move_constructor.size());
+  ASSERT_EQ(0, std_move_constructor.capacity());
   ASSERT_EQ(0, s21_move_constructor.size());
   ASSERT_EQ(0, s21_move_constructor.capacity());
-*/ 
 
+  s21_operator_overload = s21_operator_overload;
+  ASSERT_EQ(std_copy_constructor.size(), s21_operator_overload.size());
+  ASSERT_EQ(std_copy_constructor.capacity(), s21_operator_overload.capacity());
+  for (size_t i = 0; i < std_operator_overload.size(); i++)
+    ASSERT_EQ(std_operator_overload.at(i), s21_operator_overload.at(i));
+
+  std_operator_overload = std::move(std_operator_overload);
+  s21_operator_overload = std::move(s21_operator_overload);
+  ASSERT_EQ(0, std_operator_overload.size());
+  ASSERT_EQ(0, std_operator_overload.capacity());
+  ASSERT_EQ(0, s21_operator_overload.size());
+  ASSERT_EQ(0, s21_operator_overload.capacity());
+
+  ASSERT_NO_THROW(std_copy_constructor.at(4));
+  ASSERT_NO_THROW(s21_copy_constructor.at(4));
+  ASSERT_THROW(std_copy_constructor.at(5), std::out_of_range);
+  ASSERT_THROW(s21_copy_constructor.at(5), std::out_of_range);
+  try {
+    s21_operator_overload.at(5);
+  } catch (const std::out_of_range &e) {
+    ASSERT_STREQ("Incorrect input, index is outside the vector", e.what());
+  }
+
+  std::vector<int> const std_const_vector{1};
+  s21::vector<int> const s21_const_vector{1};
+  ASSERT_NO_THROW(std_const_vector.at(0));
+  ASSERT_NO_THROW(s21_const_vector.at(0));
+  ASSERT_THROW(std_const_vector.at(1), std::out_of_range);
+  ASSERT_THROW(s21_const_vector.at(1), std::out_of_range);
+  ASSERT_EQ(std_const_vector.at(0), s21_const_vector.at(0));
 }
 
 int main(int argc, char** argv) {
